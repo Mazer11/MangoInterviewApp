800f2e77d980b36fca4d8be95c4956ee75a1187f
Mazer11 <tattobu@protonmail.com>
2024-08-21 19:34:04 +0400

    App navigation and extracted strings

---
+++ core/commonui/src/main/java/org/interview/commonui/extensions/NavigationExt.kt
@@ -0,0 +1,19 @@
+package org.interview.commonui.extensions
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+
+@Composable
+inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(
+    navController: NavController
+): T {
+    val currentNavGraphRoute = destination.parent?.route ?: return hiltViewModel()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(currentNavGraphRoute)
+    }
+    return hiltViewModel(parentEntry)
+}
